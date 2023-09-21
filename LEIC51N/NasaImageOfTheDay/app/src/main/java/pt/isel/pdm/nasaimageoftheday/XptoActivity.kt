a423c7e0e50488736962bf5088f21645a59b3833
Diogo Cardoso <amazing.red@gmail.com>
2023-09-21 20:01:22 +0100

    added hello android and basic composable information

---
+++ LEIC51N/NasaImageOfTheDay/app/src/main/java/pt/isel/pdm/nasaimageoftheday/XptoActivity.kt
@@ -0,0 +1,104 @@
+package pt.isel.pdm.nasaimageoftheday
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.textInputServiceFactory
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import pt.isel.pdm.nasaimageoftheday.ui.theme.NasaImageOfTheDayTheme
+
+const val TAG: String = "NasaImageOfTheDay"
+
+class XptoActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate")
+        super.onCreate(savedInstanceState)
+        setContent {
+            NasaImageOfTheDayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+}
+
+@Composable
+fun MainScreen() {
+
+    Log.d(TAG, "MainScreen composition")
+    var showText = remember { mutableStateOf(false) }
+    Box()
+    {
+        if (showText.value == false) {
+            Button(
+                onClick = {
+                    Log.d(TAG, "Button Clicked")
+                    showText.value = true
+                    Log.d(TAG, "State is " + showText.value)
+                },
+                modifier = Modifier.align(Alignment.Center)
+            )
+            {
+                Text(text = "Load Image")
+            }
+        } else {
+            Text(text = "Here")
+        }
+
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NasaImageOfTheDayTheme {
+        Greeting("Android")
+    }
+}
--- LEIC51N/outline.md
+++ LEIC51N/outline.md
@@ -15,3 +15,33 @@ For reference:
 * Goal: Install Android Studio and create a new project (the *Hello Android* app) using Android Studio's wizard.
 * Challenge: From the miriad of questions that may arise, elect the two most relevant ones for refining your mental model of the Android platform. The professor will use them as talking points in the next class.
 
+## Week 2
+### 18/09/2023 - Building Android UIs with Jetpack Compose (part 1)
+* Android development, introduction:
+  * Identifying Android Studio project artifacts
+* Activity: introduction to the `Activity` class and its lifecycle.
+* Jetpack Compose: revisions
+  * @Composable functions: properties and constraints
+  * Stateless vs. stateful @Composables
+  * Introduction to the catalog of @Composables
+    * Elementar @Composables: `Text`, `Button`
+    * Layout @Composables: `Column`, `Row`, `Box`
+  * State management in @Composable functions
+    * `remember` - for memoizing values
+    * `mutableStateOf` - for observable mutable state
+* Automated tests in Android Studio (introduction)
+  * Instrumented tests: testing a Compose UI
+
+For reference:
+* [Thinking in Compose](https://developer.android.com/jetpack/compose/mental-model)
+* [Compose layout basics](https://developer.android.com/jetpack/compose/layouts/basics)
+* [State in Jetpack Compose](https://developer.android.com/jetpack/compose/state)
+* [Testing Jetpack Compose UIs](https://developer.android.com/jetpack/compose/testing)
+
+Video lecture:
+* Android @ ISEL - 2023 - (02) Jetpack Compose (parte 1) _(coming soon)_
+
+### 22/09/2023 - Practical class
+* Goal: Exercise building a UI with Jetpack Compose
+* Challenge 1: Build an application for counting persons entering and leaving a room. The application is comprised of a single screen that displays the current number of persons in the room and has two buttons, one for incrementing the count and another for decrementing it.
+* Project: Build the screen for displaying information about the application's authors. For now, focus on the screen's layout. No behaviour is required at this time.
